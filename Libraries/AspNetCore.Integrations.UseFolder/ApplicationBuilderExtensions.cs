38dd657a502638c2abbb04175ec64f59072dea31
Pratik Panda <mailtopratikpanda@gmail.com>
2019-01-14 09:12:30 +0530

    Added UseFolder extension.

---
+++ Libraries/AspNetCore.Integrations.UseFolder/ApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extension methods for Folder Middleware.
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Enables static file serving for the given request path.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="requestPath">The relative request path.</param>
+        /// <param name="contentRootPath">The content root path.</param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseFolder(this IApplicationBuilder app, string requestPath, string contentRootPath)
+        {
+            var fileProvider = new PhysicalFileProvider(Path.Combine(contentRootPath, requestPath));
+            var options = new StaticFileOptions
+            {
+                RequestPath = string.Concat("/", requestPath),
+                FileProvider = fileProvider
+            };
+            app.UseStaticFiles(options);
+            return app;
+        }
+    }
+}
---
+++ Libraries/AspNetCore.Integrations.UseFolder/Build.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+	1. If found, clear out any existing artifacts.
+    2. Restore the required NuGet packages.
+	3. Build the solution in Release mode.
+	4. Package all the artifacts (NuGet packages) and put them in the artifacts directory.
+#>
+
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+dotnet restore
+
+dotnet build --configuration Release
+
+dotnet pack .\Libraries\AspNetCore.Integrations.UseFolder -c Release -o .\artifacts
